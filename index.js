80c00d4f5058d6afefe5b4ba8f1e97eb0df09c82
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-05-17 15:41:23 -0500

    Adding routes table to README

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const placesRoutes = require('./controllers/places')
 const app = express()
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', placesRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
@@ -12,4 +13,6 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT)
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
