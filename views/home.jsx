b28b07e60aeaa3f365d6050f9b08b19decd8e5f5
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-05-18 18:25:09 -0500

    Adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,15 +5,15 @@ function home (){
     return(
     <Def>
         <main>
-            <h1>HOME</h1>
+            <h1>REST-Rant</h1>
             <div>
-                <img width={700} id = 'main' src='/images/chia-fruit-drink.jpg' alt='Chia Fruit Shake'/>
-                <div>
+                <img className='rounded mx-auto d-block' width={700} id = 'main' src='/images/chia-fruit-drink.jpg' alt='Chia Fruit Shake'/>
+                <div className='text-center'>
                     Photo by <a href='https://unsplash.com/@cravethebenefits'>Brenda Godinez</a> on <a href='https://unsplash.com/photos/MsTOg6rhRVk'>Unsplash</a>
                 </div>
             </div>
-            <a href='/places'>
-                <button className='btn-primary'>Places Page</button>
+            <a className='d-grid gap-2 col-6 mx-auto' href='/places'>
+                <button className='btn-primary btn-lg'>Places Page</button>
             </a>
         </main>
     </Def>
