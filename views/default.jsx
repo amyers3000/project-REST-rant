9490fc8b233ffeb184b03d9a86876bbeed012e82
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-05-18 22:22:43 -0500

    added more css

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,15 @@ function Def (html) {
     <html>
         <head>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>   
             <link rel='stylesheet' href='/css/style.css'/>
             <title>Title</title>
         </head>
         <body>
+            <div className='wrapper'>
             {html.children}
+            <div className='push'></div>
+            </div>
         </body>
     </html>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,15 +6,34 @@ function home (){
     <Def>
         <main>
             <h1>REST-Rant</h1>
-            <div>
-                <img className='rounded mx-auto d-block' width={700} id = 'main' src='/images/chia-fruit-drink.jpg' alt='Chia Fruit Shake'/>
+            <div id='img-home'>
+                <img className='rounded mx-auto d-block' width={600} id = 'main' src='/images/home.jpg' alt='Chia Fruit Shake'/>
                 <div className='text-center'>
-                    Photo by <a href='https://unsplash.com/@cravethebenefits'>Brenda Godinez</a> on <a href='https://unsplash.com/photos/MsTOg6rhRVk'>Unsplash</a>
+                    Photo by <a href='https://unsplash.com/@markuswinkler'>Markus Winkler</a> on <a href='https://unsplash.com/photos/-fRAIQHKcc0'>Unsplash</a>
                 </div>
             </div>
-            <a className='d-grid gap-2 col-6 mx-auto' href='/places'>
-                <button className='btn-primary btn-lg'>Places Page</button>
-            </a>
+            <div className="row container-home">
+                <div className="col-sm-6 box" id='places'>
+                    <div className="card">
+                    <div className="card-body">
+                        <img src="/images/review.jpg" class="card-img-top home-pic" alt="reviewer"/>
+                        <h5 className="card-title">Places to Rant About</h5>
+                        <p className="card-text">A list of various restaurants that includes locations and reviews</p>
+                        <a href="/places" class="btn  btn-dark">View List</a>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 box" id='add'>
+                    <div className="card">
+                    <div className="card-body">
+                        <img src="/images/New-Places.jpg" class="card-img-top home-pic" alt="Open sign"/>
+                        <h5 className="card-title">More to Rant?</h5>
+                        <p className="card-text"> Add more restaurants to the ever growing list - share your experience!</p>
+                        <a href="/places/new" class="btn  btn-dark">Add Place</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </Def>
     )
