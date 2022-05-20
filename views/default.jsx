b2d89fea5d54c31e0fd223cb8c180738e02b5e7f
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-05-19 19:16:10 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -4,13 +4,26 @@ function Def (html) {
     return(
     <html>
         <head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>   
+            <link rel='stylesheet' href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+            <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'/>   
             <link rel='stylesheet' href='/css/style.css'/>
             <title>Title</title>
         </head>
         <body>
             <div className='wrapper'>
+            <nav>
+                <ul>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/places'>Places</a>
+                    </li>
+                    <li>
+                        <a href='/places/new'>Add Place</a>
+                    </li>
+                </ul>
+            </nav>
             {html.children}
             <div className='push'></div>
             </div>
